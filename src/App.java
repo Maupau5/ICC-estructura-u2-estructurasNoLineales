1ecc05c2021afca1c0afd31d6f037d5653ccc393
Maupau5 <mauroipaul@gmail.com>
2025-01-08 12:43:01 -0500

    EstructurasNoLineales

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import main.Materia.Controllers.ArbolBinario;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        runArbolBinario();
     }
+
+    public static void runArbolBinario() {
+        ArbolBinario arbolBinario = new ArbolBinario();
+        int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+
+        for(int valor : valores) {
+            arbolBinario.insert(valor);
+        }
+        
+
+        arbolBinario.printTree();
+    }
+    
 }
